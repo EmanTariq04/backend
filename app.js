67026dd6c5826325a1a192e2b1ae465926011197
Eman Tariq <emantariq.1004@gmail.com>
2025-02-04 17:57:57 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectToDB = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+
+connectToDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/users", userRoutes);
+
+module.exports = app;
